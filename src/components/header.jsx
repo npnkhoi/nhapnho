e9f37ca7c6ed1a40a98d11e074009412e6341d7d
Khoi <khoinguyen.work@gmail.com>
2021-05-19 13:13:34 +0700

    Initial content adding + style fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,6 +15,24 @@ export const Header = (props) => {
                   <span></span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : 'Loading'}</p>
+                <div className="flex-row">
+                  <a
+                    href='https://www.figma.com/proto/4cHNrfbwTdFTqvppWtsl1c/Nhap-Nho?node-id=654%3A3720&scaling=scale-down&page-id=654%3A0'
+                    target="_blank"
+                    rel="noreferrer"
+                    className='demo-btn btn btn-custom btn-lg'
+                  >
+                   Demo 
+                  </a>
+                  <a
+                    href='https://docs.google.com/document/d/141UMHID_2qVQ8y68KTMRE-EcxYbRwJ9jmgEMIEhJDvk/edit?usp=sharing'
+                    target="_blank"
+                    rel="noreferrer"
+                    className='btn btn-custom btn-lg'
+                  >
+                   Full report
+                  </a>  
+                </div>
                 <a
                   href='#features'
                   className='btn btn-custom btn-lg page-scroll'
