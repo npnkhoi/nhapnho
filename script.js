7614586608e4149986abbe5014cfd3f0c79ef901
npnkhoi <npnkhoi@gmail.com>
2021-05-19 16:42:01 +0700

    update for deployment

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const fs = require("fs-extra");
 const { promisify } = require("util");
 
 async function init() {
-  await promisify(fs.remove)("./docs");
-  await promisify(fs.move)("./build", "./docs");
+  // await promisify(fs.remove)("./docs");
+  // await promisify(fs.move)("./build", "./docs");
 }
 
 init();
